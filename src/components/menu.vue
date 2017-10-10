98ab311706aa1329b21a83d3bf98906111b412e7
NeoJiang <neojiangtn@gmail.com>
2017-10-10 22:07:46 +0800

    first

---
+++ src/components/menu.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="menuContainer">
+
+  </div>
+</template>
+
+<script>
+export default {
+
+  name: 'Menu',
+
+  data () {
+    return {
+
+    };
+  }
+};
+</script>
+
+<style lang="css" scoped>
+</style>
